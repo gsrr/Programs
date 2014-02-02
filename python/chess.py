fef72b80c830db04d31dac3990d0bc1547286820
gsrr <jerrycheng1128@gmail.com>
2014-02-02 10:13:58 +0800

    update

--- python/chess.py
+++ python/chess.py
@@ -14,12 +14,58 @@ def change_matrix(matrix , loc):
         x , y = getLoc(loc)
         matrix[x][y] = 1
 
+
+def getStartPoint(x , y , matrix , loc):
+        _x , _y = getLoc(loc)
+        chessColor = matrix[_x][_y]
+        while matrix[_x][_y] == chessColor and _x + x >= 0 and _y + y >= 0:
+                _x = _x + x
+                _y = _y + y
+        return _x , _y
+
+
+def isWin(x , y , directX , directY):
+        count = 1
+        chessColor = matrix[x][y]
+        while matrix[x + directX][y + directY] == chessColor:
+                count = count + 1
+                x = x + directX
+                y = y + directY
+                if count == 5:
+                        count = 0
+                        break;
+        return count
+
+def checkLU(matrix , loc):
+        x , y = getStartPoint(-1 , -1 , matrix , loc)
+        return isWin(x , y , 1 , 1)
+
+
+def check_winner(matrix , loc):
+        ret = checkLU(matrix , loc)
+        if ret == 0:
+                return ret
+        '''
+        ret = checkUD(matrix , loc)
+        if ret == 0:
+                return ret
+        ret = checkRU(matrix , loc)
+        if ret == 0:
+                return ret
+        ret = checkRL(matrix , loc)
+        return ret
+        '''
+
 def gameStart(matrix):
         print_chessBoard(matrix)       
         while True:
                 loc = raw_input("Enter your location: ")
                 change_matrix(matrix , loc)
                 print_chessBoard(matrix)
+                ret = check_winner(matrix , loc)
+                if ret == 0:
+                        print "Winner , Game Over!!"
+                        break
                 
 
 
