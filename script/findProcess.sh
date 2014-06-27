ad4bdaef80edf4371d0d22d7ef2eef81569ecbde
gsrr <jerrycheng1128@gmail.com>
2014-06-27 17:16:11 +0800

    update

---
+++ script/findProcess.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+line='---------------------------------------------'
+pids=`ps -ef | sed 1d | awk '{print $2}'`
+
+if [ $# -eq 0 ]; then
+	read ans?"Enter port you would like to know pid for: "
+else
+	ans=$1
+fi
+
+for f in $pids
+do
+	/usr/proc/bin/pfiles $f 2>/dev/null | grep "port: $ans"
+	if [ $? -eq 0 ]; then
+		echo $line
+		echo "Port: $ans is being used by PID:\c"
+		/usr/bin/ps -ef -o pid -o args | egrep -v "grep|pfiles" | grep $f
+	fi
+done
+exit 0
