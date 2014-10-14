ffe401277f84523f32303a0cecedabc9c591b19d
gsrr <jerrycheng1128@gmail.com>
2014-10-15 00:56:10 +0800

    update

--- my_c/headfile/matrix.h
+++ my_c/headfile/matrix.h
@@ -1,45 +1,55 @@
 #include <stdio.h>
 
 
-void matrixAdd(int fir[][], int sec[][], int row, int col);
-void matrixSub(int fir[][], int sec[][], int row, int col);
-void matrixMult(int fir[][], int sec[][], int row, int col);
+void matrixAdd(Matrix fir, Matrix sec);
+void matrixSub(Matrix fir, Matrix sec);
+void matrixMult(Matrix *fir, Matrix *sec, Matrix *ret)
 
-void matrixAdd(int fir[][], int sec[][], int row, int col)
+
+typedef struct matrix{
+	int **elem;
+	int row;
+	int col;
+}Matrix;
+
+void matrixAdd(Matrix *fir, Matrix *sec)
 {
         int i;
-        for( i = 0 ; i < col ; i++ )
+        for( i = 0 ; i < fir->row ; i++ )
         {
                 int j;
-                for( j = 0 ; j < row ; j++ )
+                for( j = 0 ; j < fir->col ; j++ )
                 {
                         fir[i][j] += sec[i][j];
                 }
         }
 }
 
-void matrixSub(int fir[][], int sec[][], int row, int col)
+void matrixSub(Matrix *fir, Matrix *sec)
 {
         int i;
-        for( i = 0 ; i < col ; i++ )
+        for( i = 0 ; i < fir->row ; i++ )
         {
                 int j;
-                for( j = 0 ; j < row ; j++ )
+                for( j = 0 ; j < fir->col ; j++ )
                 {
                         fir[i][j] -= sec[i][j];
                 }
         }
 }
-void matrixMult(int fir[][], int sec[][], int ret[][], int row, int col)
+void matrixMult(Matrix *fir, Matrix *sec, Matrix *ret)
 {
         int i;
-        for( i = 0 ; i < col ; i++ )
+        for( i = 0 ; i < ret->row ; i++ )
         {
                 int j;
-                int ret = 0;
-                for( j = 0 ; j < row ; j++ )
+                for( j = 0 ; j < ret->col ; j++ )
                 {
-                        ret += fir[i][j] * sec[j][i];
+			int n;
+			for( n = 0 ; n < fir->col ; n++)
+			{
+				ret->elem[i][j] += fir[i][n] * sec[n][j];
+			}
                 }
                 
         }
