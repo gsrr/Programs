8318c5b7f7730f2a00cae8ca6b5360029c61b472
gsrr <jerrycheng1128@gmail.com>
2014-11-14 11:07:18 +0800

    update

--- java/reflection/Handler.java
+++ java/reflection/Handler.java
@@ -26,6 +26,11 @@ public class Handler
                         Object obj = constructor.newInstance(number);
                         System.out.println(objAdd.invoke(obj));
                         System.out.println(objSub.invoke(obj, 20, 5));
+                        
+                        Library lib = new Library(new Number());
+                        java.lang.reflect.Method libAdd = myClass.getMethod("addition");
+                        System.out.println(libAdd.invoke(lib));
+
                 }
                 catch(Exception e)
                 {
