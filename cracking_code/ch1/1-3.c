afd17697133a33519836d2c81e0d3233fd84dad4
gsrr <jerrycheng1128@gmail.com>
2015-03-26 15:06:22 +0800

    update 1-4

--- cracking_code/ch1/1-3.c
+++ cracking_code/ch1/1-3.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /* ::#def:void generateRandomString(char *charSet, int length, char *str):: */
 void generateRandomString(char *charSet, int length, char *str)
@@ -17,12 +18,61 @@ void generateRandomString(char *charSet, int length, char *str)
         str[length - 1] = '\0';
 }
 
+
+int checkPerm_buffer(char* str1, char* str2)
+{
+        int len1 = strlen(str1);
+        int len2 = strlen(str2);
+        if(len1 != len2)
+        {
+                return -1;
+        }
+
+        int buffer[128] = {0};
+        int i;
+        for( i = 0 ; i < len1 ; i++ )
+        {
+                buffer[str1[i]]++;       
+        }     
+        for( i = 0 ; i < len2 ; i++)
+        {
+                buffer[str2[i]]--;
+                if(buffer[str2[i]] == -1)
+                {
+                        return -1;
+                }
+        }
+        
+        return 0;
+}
+
+/*int as_compare (const void * a, const void * b)*/
+int as_compare (const void * a, const void * b)
+{
+  return ( *(char*)a - *(char*)b );
+}
+
+int checkPerm_sort(char* str1, char* str2)
+{
+        int len1 = strlen(str1);
+        int len2 = strlen(str2);
+        qsort(str1, len1, sizeof(char), as_compare);       
+        qsort(str2, len2, sizeof(char), as_compare);    
+        printf("%s\n%s\n", str1,str2);
+        return strcmp(str1, str2);   
+}
+
 int main()
 {
         char charSet[] = "abcdefghijklmnopqrstuvwxyz";
-        char str[10] = {0};
-        generateRandomString(charSet , 10 , str);
-        printf("%s\n", str);   
+        char str1[10] = {0};
+        char str2[10] = {0};
+        generateRandomString(charSet , 10 , str1);
+        generateRandomString(charSet , 10 , str2);
+        
+        printf("%s\n%s\n", str1, str2);  
+        printf("checkPerm_buffer : %d\n", checkPerm_buffer(str1, str2)); 
+        printf("checkPerm_sort : %d\n", checkPerm_sort(str1, str2)); 
         return 0;
 }
 
--- cracking_code/ch1/1-4.c
+++ cracking_code/ch1/1-4.c
@@ -1,11 +1,88 @@
-
+/* #replace space*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
+/* ::#def:void generateRandomString(char *str, int len):: */
+void generateRandomString(char *str, int len)
+{
+        char charSet[] = "abcdefghijklminopqrstuvwxyz "; 
+        int setSize = sizeof(charSet);
+        srand(time(NULL));
+        int i;
+        for( i = 0 ; i < len -1 ; i++)
+        {
+                int r = rand() % setSize;
+                str[i] = charSet[r];
+        }
+        str[len - 1] = '\0';
+}
+
+char* expandString(char *str, int len)
+{
+        int cnt = 0;
+        int i;
+        for( i = 0 ; i < len ; i++ )
+        {
+                if(str[i] == ' ')
+                {
+                        cnt++;
+                }
+        } 
+        int ex_len = len + cnt * 2;
+        char *str_ex = (char*)malloc(sizeof(char) * ex_len);
+        strcpy(str_ex, str);
+
+        i = len - 1;
+        for( i ; i < ex_len - 1 ; i++)
+        {
+                str_ex[i] = ' ';
+        }
+        str_ex[ex_len - 1] = '\0'; 
+        return str_ex;
+}
+
+
+void replaceSpace(char* str, int len)
+{
+        int cnt = 0;
+        int i;
+        for(i = 0 ; i < len ; i++)
+        {
+                if(str[i] == ' ')
+                {
+                        cnt++;
+                }       
+        }
+        i = 0;
+        for( i = len - 1 ; i > -1 ; i--)
+        {
+                if(str[i] == ' ')
+                {
+                        cnt--;
+                        str[i + 2 * cnt] = '%';
+                        str[i + 2 * cnt + 1] = '2';
+                        str[i + 2 * cnt + 2] = '0';
+                }
+                else
+                {
+                        str[i + 2 * cnt] = str[i];
+                }
+        }
+        i = 0 ;
+}
+
 int main()
 {
+        char str[10];
+        generateRandomString(str, 10);
+        printf("final:%s:%d\n", str, strlen(str));
+        char *str_ex = expandString(str, 10);
+        printf("final:%s:%d\n", str_ex, strlen(str_ex));
+        replaceSpace(str_ex, strlen(str));
+        printf("final:%s:%d\n", str_ex, strlen(str_ex));
+
         return 0;
 }
 
