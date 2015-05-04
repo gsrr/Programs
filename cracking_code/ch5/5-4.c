db0b94d99d2f397b96d0f1c8716c963f260f6abb
gsrr <jerrycheng1128@gmail.com>
2015-05-04 13:46:41 +0800

    update

--- cracking_code/ch5/5-4.c
+++ cracking_code/ch5/5-4.c
@@ -9,8 +9,56 @@
 #include <unistd.h>
 #include <math.h>
 
+
+void isPowerOf2(int num)
+{
+        if( (num & (num-1)) == 0)
+        {
+                printf("%d is power of 2 : True\n", num);
+        }
+        else
+        {
+                printf("%d is power of 2 : False\n", num);
+        }
+}
+
+
+
+
+/* ::#def myrand :: */
+typedef struct{
+        int base;
+        int cust;
+}f_args;
+
+int myrand_base(int base)
+{
+        return rand() % base;
+}
+
+int var_f(f_args in)
+{
+        int base = in.cust ? in.cust : in.base;
+        return myrand_base(base);
+}
+
+/*
+* varadic macros
+*/
+#define myrand(...) var_f((f_args){.base=1000,__VA_ARGS__});
+
+
+/* #end */
+
 int main()
 {
+
+        int num = rand() % 100;
+        isPowerOf2(num);
+        isPowerOf2(4);
+        int num2 = myrand(100);
+        isPowerOf2(num2);
+
         return 0;
 }
 
---
+++ cracking_code/ch5/5-5.c
@@ -0,0 +1,16 @@
+
+/*
+ * determine the number of bits need to flip to convert integer A to integer B
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+
+int main()
+{
+        return 0;
+}
+
